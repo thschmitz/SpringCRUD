295802518641a30f7306488d4c7bd5a12d033999
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-04 15:29:30 -0300

    Insert Users

--- src/main/java/com/thschmitz/workshopmongo/services/UserService.java
+++ src/main/java/com/thschmitz/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thschmitz.workshopmongo.domain.User;
+import com.thschmitz.workshopmongo.dto.UserDTO;
 import com.thschmitz.workshopmongo.repository.UserRepository;
 import com.thschmitz.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 	
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
