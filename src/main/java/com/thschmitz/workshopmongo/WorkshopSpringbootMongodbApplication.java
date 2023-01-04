d5c38d009b3738dd0672adf7918780e150d3ef5e
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-03 21:24:57 -0300

    InitialCommit

---
+++ src/main/java/com/thschmitz/workshopmongo/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.thschmitz.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+
+}
